b0aeb89b215d2e0e16de7363e010e8b0983c66a9
Kat Leight <leightkt@gmail.com>
2021-02-04 11:59:27 -0700

    added evaluate and clear function

--- app.js
+++ app.js
@@ -1,48 +1,62 @@
-document.addEventListener('DOMContentLoaded', function(){
-    const $screen = document.querySelector("#screen")
-    const $clear = document.querySelector("#clear")
-    const $equals = document.querySelector("#equals")
-    const $spans = document.querySelectorAll("span")
-    const $operators = document.querySelectorAll(".operator")
-    const userInput = ""
+let userInput = []
+const $screen = document.querySelector("#screen")
+const $clear = document.querySelector("#clear")
+const $equals = document.querySelector("#equals")
+const $spans = document.querySelectorAll("span")
 
+// document.addEventListener('DOMContentLoaded', function(){
+//     $screen = document.querySelector("#screen")
+//     $clear = document.querySelector("#clear")
+//     $equals = document.querySelector("#equals")
+//     $spans = document.querySelectorAll("span")
+// })
+
+startCalculator()
+
+function startCalculator(){
+    addClearAction($clear, $screen, userInput)
     addClickToSpans($spans, $screen, userInput)
-    addClearAction($clear, $screen)
     addEqualsAction($equals, $screen, userInput)
-})
+}
 
-function addClickToSpans(spans, screen, userInput){
-    spans.forEach(span => {
+function addClickToSpans($spans, $screen, userInput){
+    $spans.forEach(span => {
         if (span.id !== 'clear' && span.id !== 'equals'){
             span.addEventListener('click', (event)=>{
-                displayClickValue(event.target, screen)
-                userInput += event.target.innerText
+                displayClickValue(event.target, $screen)
+                userInput.push(event.target.innerText)
+                console.log(userInput)
             })
         }
     })
 }
 
-function displayClickValue(span, screen){
-    screen.innerText = span.innerText
+function displayClickValue(span, $screen){
+    $screen.innerText = span.innerText
 }
 
-function addClearAction(clear, screen){
-    clear.addEventListener('click', (event)=>{
-        screen.innerText = ""
-        userInput = ""
+function addClearAction($clear, $screen, userInput){
+    $clear.addEventListener('click', (event)=>{
+        $screen.innerText = ""
+        userInput = clearInput()
     })
 }
 
-function addEqualsAction(equals, screen, userInput){
-    const operators = ["+", "-", "x", "÷"]
-    userInput.split()
-    equals.addEventListener('click', (event)=>{
-        operators.find(operator => {
-            if (userInput[1] === operator){
-                screen.innerText = `${userInput[0]}${operator}${userInput[2]}`
-            }
-        })
+function addEqualsAction($equals, $screen, userInput, operatorValues){
+    $equals.addEventListener('click', (event)=>{
+        console.log(userInput)
+        if (eval(userInput.join(' '))){
+            $screen.innerText = eval(userInput.join(' '))
+        } else {
+            $screen.innerText
+        }
+        userInput = clearInput()
     })
 }
 
-// if (Number.isInteger(parseInt(span.innerText))){
+function clearInput(){
+    while (userInput.length > 0){
+        userInput.pop()
+    }
+    return userInput
+}
